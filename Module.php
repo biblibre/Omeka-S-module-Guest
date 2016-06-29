f2844aabe888f09ee11cd2b22aba4504eb8ceae1
efalcy <efalcy@afi-sa.fr>
2016-06-29 09:40:26 +0200

    Remove unused files / methods

--- Module.php
+++ Module.php
@@ -1,5 +1,34 @@
 <?php
+/*
+ * Copyright BibLibre, 2016
+ *
+ * This software is governed by the CeCILL license under French law and abiding
+ * by the rules of distribution of free software.  You can use, modify and/ or
+ * redistribute the software under the terms of the CeCILL license as circulated
+ * by CEA, CNRS and INRIA at the following URL "http://www.cecill.info".
+ *
+ * As a counterpart to the access to the source code and rights to copy, modify
+ * and redistribute granted by the license, users are provided only with a
+ * limited warranty and the software's author, the holder of the economic
+ * rights, and the successive licensors have only limited liability.
+ *
+ * In this respect, the user's attention is drawn to the risks associated with
+ * loading, using, modifying and/or developing or reproducing the software by
+ * the user in light of its specific status of free software, that may mean that
+ * it is complicated to manipulate, and that also therefore means that it is
+ * reserved for developers and experienced professionals having in-depth
+ * computer knowledge. Users are therefore encouraged to load and test the
+ * software's suitability as regards their requirements in conditions enabling
+ * the security of their systems and/or data to be ensured and, more generally,
+ * to use and operate it in the same conditions as regards security.
+ *
+ * The fact that you are presently reading this means that you have had
+ * knowledge of the CeCILL license and that you accept its terms.
+ */
+
+
 namespace GuestUser;
+
 use Omeka\Module\AbstractModule;
 use Zend\ServiceManager\ServiceLocatorInterface;
 use Zend\View\Renderer\PhpRenderer;
@@ -14,9 +43,6 @@ use Zend\Permissions\Acl\Acl;
 use Zend\Permissions\Acl\Role\GenericRole;
 use Zend\Permissions\Acl\Resource\GenericResource;
 use Omeka\Event\Event;
-//use Omeka\Event\Event;
-
-//include(FORM_DIR . '/User.php');
 
 
 class Module extends AbstractModule
@@ -69,7 +95,11 @@ class Module extends AbstractModule
 
     public function uninstall(ServiceLocatorInterface $serviceLocator)
     {
-        //deactivate the guest users
+        $this->deactivateGuestUsers($serviceLocator);
+    }
+
+
+    protected function deactivateGuestUsers($serviceLocator) {
         $em = $serviceLocator->get('Omeka\EntityManager');
         $guestUsers = $em->getRepository('Omeka\Entity\User')->findBy(['role'=>'guest']);
         foreach($guestUsers as $user) {
@@ -78,7 +108,6 @@ class Module extends AbstractModule
             $em->flush();
         }
     }
-
 
 
     public function handleConfigForm(AbstractController $controller)
@@ -99,31 +128,6 @@ class Module extends AbstractModule
 
     }
 
-    public function hookAdminThemeHeader($args)
-    {
-        $request = $args['request'];
-        if($request->getControllerName() == 'plugins' && $request->getParam('name') == 'GuestUser') {
-            queue_js_file('tiny_mce/tiny_mce');
-            $js = "if (typeof(Omeka) !== 'undefined'){
-                Omeka.wysiwyg();
-            };";
-            queue_js_string($js);
-        }
-
-    }
-
-
-    public function filterPublicShowAdminBar($show)
-    {
-        return true;
-    }
-
-    public function filterAdminNavigationMain($navLinks)
-    {
-        $navLinks['Guest User'] = array('label' => $this->translate("Guest Users"),
-                                        'uri' => url("guest-user/user/browse?role=guest"));
-        return $navLinks;
-    }
 
     public function setConfig($config) {
         $this->config=$config;
@@ -158,7 +162,6 @@ class Module extends AbstractModule
     }
 
     public function translate($string,$options='',$serviceLocator=null) {
-
         if (!$serviceLocator)
             $serviceLocator = $this->getServiceLocator();
 
