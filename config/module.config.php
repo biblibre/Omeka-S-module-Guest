4bb678da11eafb9bfc5ecabba55a7ac6efcea95b
Daniel Berthereau <Daniel.github@Berthereau.net>
2020-03-16 00:00:00 +0100

    Updated default messages of emails.

--- config/module.config.php
+++ config/module.config.php
@@ -151,17 +151,17 @@ return [
             'guest_short_capabilities' => '',
             'guest_message_confirm_email_subject' => '[{site_title}] Confirm email', // @translate
             'guest_message_confirm_email' => '<p>Hi {user_name},</p>
-<p>You have registered for an account on {main_title} / {site_title} ({site_url}).</p>
-<p>Please confirm your registration by following this link: {token_url}.</p>
-<p>If you did not request to join {main_title} please disregard this email.</p>', // @translate
+<p>You have registered for an account on <a href="{site_url}">{site_title}</a> ({main_title}).</p>
+<p>Please confirm your registration by following this link: <a href="{token_url}"> {token_url}</a></p>
+<p>If you did not request to join {main_title} / {site_title}, please disregard this email.</p>', // @translate
             'guest_message_confirm_registration_email_subject' => '[{site_title}] Account open', // @translate
             'guest_message_confirm_registration_email' => '<p>Hi {user_name},</p>
-<p>We are happy to open your account on {main_title} / {site_title} ({site_url}).</p>
+<p>We are happy to open your account on <a href="{site_url}">{site_title}</a> ({main_title}).</p>
 <p>You can now login and discover the site.</p>', // @translate
             'guest_message_update_email_subject' => '[{site_title}] Confirm email', // @translate
             'guest_message_update_email' => '<p>Hi {user_name},</p>
-<p>You have requested to update email on {main_title} / {site_title} ({site_url}).</p>
-<p>Please confirm your email by following this link: {token_url}.</p>
+<p>You have requested to update email on <a href="{site_url}">{site_title}</a> ({main_title}).</p>
+<p>Please confirm your email by following this link: <a href="{token_url}">{token_url}</a>.</p>
 <p>If you did not request to update your email on {main_title}, please disregard this email.</p>', // @translate
             'guest_message_confirm_email_site' => 'Your email "{email}" is confirmed for {site_title}.', // @translate
             'guest_message_confirm_register_site' => 'Thank you for registering. Please check your email for a confirmation message. Once you have confirmed your request, you will be able to log in.', // @translate
@@ -181,17 +181,17 @@ return [
             'guest_short_capabilities' => '',
             'guest_message_confirm_email_subject' => '[{site_title}] Confirm email', // @translate
             'guest_message_confirm_email' => '<p>Hi {user_name},</p>
-<p>You have registered for an account on {main_title} / {site_title} ({site_url}).</p>
-<p>Please confirm your registration by following this link: {token_url}.</p>
-<p>If you did not request to join {main_title} please disregard this email.</p>', // @translate
+<p>You have registered for an account on <a href="{site_url}">{site_title}</a> ({main_title}).</p>
+<p>Please confirm your registration by following this link: <a href="{token_url}"> {token_url}</a></p>
+<p>If you did not request to join {main_title} / {site_title}, please disregard this email.</p>', // @translate
             'guest_message_confirm_registration_email_subject' => '[{site_title}] Account open', // @translate
             'guest_message_confirm_registration_email' => '<p>Hi {user_name},</p>
-<p>We are happy to open your account on {main_title} / {site_title} ({site_url}).</p>
+<p>We are happy to open your account on <a href="{site_url}">{site_title}</a> ({main_title}).</p>
 <p>You can now login and discover the site.</p>', // @translate
             'guest_message_update_email_subject' => '[{site_title}] Confirm email', // @translate
             'guest_message_update_email' => '<p>Hi {user_name},</p>
-<p>You have requested to update email on {main_title} / {site_title} ({site_url}).</p>
-<p>Please confirm your email by following this link: {token_url}.</p>
+<p>You have requested to update email on <a href="{site_url}">{site_title}</a> ({main_title}).</p>
+<p>Please confirm your email by following this link: <a href="{token_url}">{token_url}</a>.</p>
 <p>If you did not request to update your email on {main_title}, please disregard this email.</p>', // @translate
             'guest_message_confirm_email_site' => 'Your email "{email}" is confirmed for {site_title}.', // @translate
             'guest_message_confirm_register_site' => 'Thank you for registering. Please check your email for a confirmation message. Once you have confirmed your request, you will be able to log in.', // @translate
